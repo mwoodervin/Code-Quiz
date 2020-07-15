1b0fbddfcbd9b07dc5c2fd70ff9a6546f350a47e
Mary Wood <mwoodervin@gmail.com>
2020-07-15 09:09:22 -0400

    javascript pseudo code

---
+++ script.js
@@ -0,0 +1,58 @@
+// STEPS FOR JS FILE
+
+// set variables for time : let timeEl = 
+//score
+// user initials
+// sections of the html document
+    // how to use this quiz
+    // questions
+    // final message - quiz is done
+    // div with correct/incorrect for each answer
+
+// buttons
+    // start
+    // answer 1
+    // answer 2
+    // answer 3
+    // answer 4 
+    // answer 5
+
+// Object to compare quiz answer with user answer (true/false)
+
+// Object for questions and answers
+
+// Listening events
+    // start quiz
+        // go to question 1
+        // show question 1
+        // show answer options 1-4
+    // start timer
+
+// click answer 
+    // if correct show Yay!
+    // if incorrect show Boo!
+    // if incorrect deduct 10 secs from time
+    // set question next
+        // set question
+        // set answer options 1-4
+        // loop through to last quesiton
+
+// After last question OR timer = 0
+    // stop timer
+    // show final messag - quiz is done
+    // store score/time to local storage
+
+// Click submit to enter initials
+    // show high score html page
+    // add this score to high score object
+    // High scores should be ordered from high to low
+
+// click to go back to html.index/try again
+    // land on intro/startup for quiz page
+
+// click clear scores button
+    // clear the high scores array/object
+
+// click to view high scores from html.index
+    // go to high score page
+
